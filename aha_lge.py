63046f5ce24f7d97de6b5d900e8bac850fc6b40c
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-07-18 19:32:11 +0100

    Update aha_lge.py

--- aha_lge.py
+++ aha_lge.py
@@ -26,6 +26,7 @@ from random import sample
 # Command line arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, help="path to input directory")
+ap.add_argument("-t", "--target", required=False, help="name of the target field")
 args = vars(ap.parse_args())
 
 # Set parameters
@@ -97,43 +98,43 @@ aha_list = ['LGE_basal anterior','LGE_basal anteroseptum','LGE_basal inferoseptu
 #df['aha_lge'] = np.array(aha_list) #df[aha_list].apply(lambda x:'{}'.format(np.max(x)), axis=1)
 #df['aha_lge'] = df['aha_lge'].astype(int)
 
+
 (df_train, df_valid) = train_test_split(df, train_size=0.8, random_state=42)
 
-#p_ind_train = df_train[df_train['Labels']==1].index.tolist()
-#np_ind_train = df_train[df_train['Labels']==0].index.tolist()
-#np_sample_train = sample(np_ind_train, len(p_ind_train))
-#df_train = df_train.loc[p_ind_train + np_sample_train]
-#p_ind_valid = df_valid[df_valid['Labels']==1].index.tolist()
-#np_ind_valid = df_valid[df_valid['Labels']==0].index.tolist()
-#np_sample_valid = sample(np_ind_valid, 11*len(p_ind_valid))
-#df_valid = df_valid.loc[p_ind_valid + np_sample_valid]
-
-X_train = np.array([x // 255.0 for x in df_train['LGE']])
-print(X_train.shape)
-X_valid = np.array([x // 255.0 for x in df_valid['LGE']])
-print(X_valid.shape)
-
 y_train = np.array(df_train[aha_list])
+y_train[y_train == 2] = 1
 #tlist = y_train.tolist()
 #print(tlist.count(1))
 y_valid = np.array(df_valid[aha_list])
+y_valid[y_valid == 2] = 1
 #vlist = y_valid.tolist()
 #print(vlist.count(1))
-print(y_train[:5])
-print(y_valid[:5])
+#p_ind_train = df_train[df_train[args["target"]]==1].index.tolist()
+#np_ind_train = df_train[df_train[args["target"]]==0].index.tolist()
+#np_sample_train = sample(np_ind_train, len(p_ind_train))
+#df_train = df_train.loc[p_ind_train + np_sample_train]
+#trainy = np.array(df_train[args["target"]])
+#p_ind_valid = df_valid[df_valid[args["target"]]==1].index.tolist()
+#np_ind_valid = df_valid[df_valid[args["target"]]==0].index.tolist()
+#np_sample_valid = sample(np_ind_valid, ((len(df_valid) - vlist.count(1)) // vlist.count(1))*len(p_ind_valid))
+#df_valid = df_valid.loc[p_ind_valid + np_sample_valid]
+#validy = np.array(df_valid[args["target"]])
 
-class_weight = {0: 1,
-                1: 11}
+X_train = np.array([x for x in df_train['LGE']])
+print(X_train.shape)
+X_valid = np.array([x for x in df_valid['LGE']])
+print(X_valid.shape)
+print(y_train[:20])
+print(y_valid[:20])
+
+#class_weight = {0: 1.0,
+ #               1: (tlist.count(0) + vlist.count(0)) / (tlist.count(1) + vlist.count(1))}
 
 # Data augmentation
 aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True,rotation_range=20, width_shift_range=0.1,
                          height_shift_range=0.1,shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
-#X_train = aug.flow(X_train, batch_size=2000)
-#X_train = X_train.next()
 
 v_aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True)
-#X_valid = v_aug.flow(X_valid, batch_size=1000)
-#X_valid = X_valid.next()
 
 # Initialise the optimiser and model
 print("[INFO] compiling model ...")
@@ -159,7 +160,7 @@ model = tf_cnns.LeNet.build(INPUT_DIM, INPUT_DIM, depth=1, classes=N_CLASSES)
 my_model.compile(loss=Loss, optimizer=Opt, metrics=METRICS)
 weigth_path = "{}_my_model.best.hdf5".format("models/aha_VGG19")
 checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
-early = EarlyStopping(monitor='val_prc', mode='max', patience=30)
+early = EarlyStopping(monitor='val_prc', mode='max', patience=20)
 callbacks_list = [checkpoint]
 
 logdir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -167,8 +168,9 @@ tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
 
 # Training the model
 print("[INFO] Training the model ...")
-history = my_model.fit(X_train, y_train, validation_data= (X_valid, y_valid), batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
-                  steps_per_epoch=len(X_train )// BATCH_SIZE, callbacks=[early,callbacks_list, tensorboard_callback], verbose=1)
+history = my_model.fit_generator(aug.flow(X_train, y_train, batch_size=BATCH_SIZE), validation_data=v_aug.flow(X_valid, y_valid),
+                          epochs=NUM_EPOCHS,callbacks=[early, checkpoint, tensorboard_callback],
+                                  verbose=1)
 
 # summarize history for loss
 plt.plot(history.history['prc'])
