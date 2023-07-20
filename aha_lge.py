ff94b2b991e6ffe813597b53d87678fa3fe12b41
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-07-20 23:46:02 +0100

    Update aha_lge.py

--- aha_lge.py
+++ aha_lge.py
@@ -9,6 +9,7 @@ import tf_cnns
 import argparse
 from sklearn.model_selection import train_test_split
 from skimage.transform import resize
+from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam, SGD, RMSprop
@@ -35,7 +36,7 @@ WIDTH = 224
 HEIGHT = 2912
 BATCH_SIZE = 32
 NUM_EPOCHS = 500
-N_CLASSES = 16
+N_CLASSES = 1
 
 # Load dataframe and creating classes
 patient_info = pd.read_csv('/Users/ebrahamalskaf/Documents/patient_info.csv')
@@ -62,7 +63,7 @@ else:
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet import ResNet50
 model = VGG16(include_top=False, input_shape=(HEIGHT, WIDTH, 3), weights='imagenet')
-#model = ResNet50(include_top=False, input_shape=(224, 224, 3), weights='imagenet')
+#model = ResNet50(include_top=False, input_shape=(HEIGHT, WIDTH, 3), weights='imagenet')
 
 transfer_layer = model.get_layer('block5_pool')
 vgg_model = Model(inputs = model.input, outputs = transfer_layer.output)
@@ -95,33 +96,38 @@ aha_list = ['LGE_basal anterior','LGE_basal anteroseptum','LGE_basal inferoseptu
                         ,'LGE_basal inferolateral', 'LGE_basal anterolateral','LGE_mid anterior','LGE_mid anteroseptum','LGE_mid inferoseptum','LGE_mid inferior',
                                        'LGE_mid inferolateral','LGE_mid anterolateral','LGE_apical anterior', 'LGE_apical septum','LGE_apical inferior','LGE_apical lateral']
 
-#df['aha_lge'] = np.array(aha_list) #df[aha_list].apply(lambda x:'{}'.format(np.max(x)), axis=1)
-#df['aha_lge'] = df['aha_lge'].astype(int)
+df['aha_lge'] = df[aha_list].apply(lambda x:'{}'.format(np.sum(x)), axis=1)
+df['aha_lge'] = df['aha_lge'].astype(int)
 
 
-(df_train, df_valid) = train_test_split(df, train_size=0.8, random_state=42)
+(df_train, df_valid) = train_test_split(df, train_size=0.8, stratify=df[args["target"]], shuffle=True)
 
-y_train = np.array(df_train[aha_list])
-y_train[y_train == 2] = 1
-y_valid = np.array(df_valid[aha_list])
+df_train[args["target"]] = df[args["target"]].replace(to_replace = 2, value =1)
+p_ind_train = df_train[df_train[args["target"]] == 1].index.tolist()
+np_ind_train = df_train[df_train[args["target"]] == 0].index.tolist()
+np_sample_train = sample(np_ind_train, len(p_ind_train))
+df_train = df_train.loc[p_ind_train + np_sample_train]
+y_train = np.array(df_train[args["target"]])
+tlist = y_train.tolist()
+print(tlist.count(1))
+
+y_valid = np.array(df_valid[args["target"]])
 y_valid[y_valid == 2] = 1
-#p_ind_train = df_train[df_train[args["target"]]==1].index.tolist()
-#np_ind_train = df_train[df_train[args["target"]]==0].index.tolist()
-#np_sample_train = sample(np_ind_train, len(p_ind_train))
-#df_train = df_train.loc[p_ind_train + np_sample_train]
-#trainy = np.array(df_train[args["target"]])
-#p_ind_valid = df_valid[df_valid[args["target"]]==1].index.tolist()
-#np_ind_valid = df_valid[df_valid[args["target"]]==0].index.tolist()
-#np_sample_valid = sample(np_ind_valid, ((len(df_valid) - vlist.count(1)) // vlist.count(1))*len(p_ind_valid))
-#df_valid = df_valid.loc[p_ind_valid + np_sample_valid]
-#validy = np.array(df_valid[args["target"]])
+vlist = y_valid.tolist()
+print(vlist.count(1))
 
 X_train = np.array([x for x in df_train['LGE']])
+#oversample = SMOTE()
+#X_train, y_train = oversample.fit(X_train, y_train)
 print(X_train.shape)
 X_valid = np.array([x for x in df_valid['LGE']])
 print(X_valid.shape)
-print(y_train[:20])
-print(y_valid[:20])
+print(y_train[:5])
+print(y_valid[:5])
+
+#print((tlist.count(0)+vlist.count(0)) / (tlist.count(1)+vlist.count(1)))
+#class_weight = {0: 1.0,
+ #               1: (tlist.count(0)+vlist.count(0)) / (tlist.count(1)+vlist.count(1))}
 
 #class_weight = {0: 1.0,
  #               1: (tlist.count(0) + vlist.count(0)) / (tlist.count(1) + vlist.count(1))}
@@ -154,7 +160,7 @@ def my_tf_loss_fn(y_true, y_pred):
 Loss = BinaryCrossentropy()
 model = tf_cnns.LeNet.build(INPUT_DIM, INPUT_DIM, depth=1, classes=N_CLASSES)
 my_model.compile(loss=Loss, optimizer=Opt, metrics=METRICS)
-weigth_path = "{}_my_model.best.hdf5".format("models/aha_VGG19")
+weigth_path = "{}_my_model.best.hdf5".format("models/aha1_VGG19")
 checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
 early = EarlyStopping(monitor='val_prc', mode='max', patience=20)
 callbacks_list = [checkpoint]
@@ -166,7 +172,7 @@ tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
 print("[INFO] Training the model ...")
 history = my_model.fit_generator(aug.flow(X_train, y_train, batch_size=BATCH_SIZE), validation_data=v_aug.flow(X_valid, y_valid),
                           epochs=NUM_EPOCHS,callbacks=[early, checkpoint, tensorboard_callback],
-                                  verbose=1)
+                           verbose=1)
 
 # summarize history for loss
 plt.plot(history.history['prc'])
@@ -181,7 +187,7 @@ plt.show()
 
 # Saving model data
 model_json = my_model.to_json()
-with open("models/aha_VGG19.json", "w") as json_file:
+with open("models/aha1_VGG19.json", "w") as json_file:
     json_file.write(model_json)
 
 K.clear_session()
